98b192ed8cfd203155af88d456378b8cd9359d98
Damien <mail@damientimewell.com>
2018-01-16 10:48:53 +0100

    Showing a possible login solution

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import express from "express"
 import bodyParser from "body-parser"
 import cors from "cors"
 import bcrypt from "bcrypt-nodejs"
+import uuid from "uuid/v4"
 
 // Express setup, including JSON body parsing.
 const app = express()
@@ -23,19 +24,57 @@ mongoose.Promise = Promise
 mongoose.connection.on("error", err => console.error("Connection error:", err))
 mongoose.connection.once("open", () => console.log("Connected to mongodb"))
 
-//
-// Define a model here.
-//
+const User = mongoose.model("User", {
+  username: {
+    type: String,
+    unique: true
+  },
+  password: String,
+  accessToken: {
+    type: String,
+    default: () => uuid()
+  }
+})
 
 // Example root endpoint to get started with
 app.get("/", (req, res) => {
   const password = "supersecretpassword"
   const hash = bcrypt.hashSync(password)
 
-  // bcrypt.compareSync("supersecretpassword", hash) // true
+  // bcrypt.compareSync(req.body.password, user.password) // true
   // bcrypt.compareSync("incorrectpassword", hash) // false
 
   res.send(`Signup form api. Here's an example of an encrypted password: ${hash}`)
+})
+
+app.post("/users", (req, res) => {
+  const { username } = req.body
+  const password = bcrypt.hashSync(req.body.password)
+  const user = new User({ username, password })
+
+  user.save()
+    .then(() => res.status(201).json(user))
+    .catch(err => res.status(400).json(err))
+})
+
+app.post("/login", (req, res) => {
+  // 1. Find the user -
+  // 2. Check if the user existed? -
+  // 3. Check if the password matched? -
+  User.findOne({ username: req.body.username })
+    .then(user => {
+      console.log(user)
+      if (user && bcrypt.compareSync(req.body.password, user.password)) {
+        res.json(user)
+      } else {
+        // res.status(401).send("Unauthenticated")
+        res.status(401).json({
+          errors: {
+            username: "Username is invalid"
+          }
+        })
+      }
+    })
 })
 
 // Add more endpoints here!
--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,11 +1,33 @@
 import React from "react"
+import Login from "./login"
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      accessToken: "",
+      userId: ""
+    }
+  }
+
+  handleLoginSuccess = user => {
+    this.setState({
+      accessToken: user.accessToken,
+      userId: user._id
+    })
+  }
+
+  isLoggedIn = () => (
+    this.state.accessToken && this.state.userId
+  )
 
   render() {
     return (
       <div>
-        Find me in src/app.js!
+        {this.isLoggedIn()
+          ? <h1>Welcome!</h1>
+          : <Login onLoginSuccess={this.handleLoginSuccess} />}
       </div>
     )
   }
